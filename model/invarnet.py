a1bdebd5d517a9956bbb9ed1b0eded48e37d05d8
Chaitanya Pankajkumar Patel <chpatel@node4-ccn2cluster.stanford.edu>
2023-03-19 22:16:56 -0700

    pyg added

--- model/invarnet.py
+++ model/invarnet.py
@@ -2,11 +2,12 @@ import torch
 import torch.nn as nn
 from pytorch3d.transforms.rotation_conversions import quaternion_to_matrix, rotation_6d_to_matrix
 import torch_scatter
+import torch_geometric as pyg
 
 from model.core import MLP, get_normalization
 
 
-class InvarNet(nn.Module):
+class InvarNet(pyg.nn.MessagePassing):
     def __init__(self, cfg):
 
         super(InvarNet, self).__init__()
@@ -51,8 +52,9 @@ class InvarNet(nn.Module):
             self.node_encoder_input_norm = get_normalization(self.inp_normalization)(self.node_feat_dim)
             self.rel_encoder_input_norm = get_normalization(self.inp_normalization)(self.rel_feat_dim)
 
+        self.use_indexing=True
 
-    def forward(self, node_feats, rels, rel_feats, rel_stages):
+    def forward_old(self, node_feats, rels, rel_feats, rel_stages):
         # node_feats: N x node_feat_dim
         # rel_feats: N x rel_feat_dim
         # rels: R x 2
@@ -92,3 +94,54 @@ class InvarNet(nn.Module):
         out = self.node_dec(node_latents)
         return out
 
+
+    def forward(self, node_feats, rels, rel_feats, rel_stages):
+        # node_feats: N x node_feat_dim
+        # rel_feats: N x rel_feat_dim
+        # rels: R x 2
+        # rel_stages: R
+        # instance_idx: O+1
+        if self.use_indexing:
+            return self.forward_old(node_feats, rels, rel_feats, rel_stages)
+
+        num_nodes = node_feats.shape[0]
+        num_rels = rels.shape[0]
+
+        if self.inp_normalization != 'none':
+            node_feats = self.node_encoder_input_norm(node_feats)
+            rel_feats = self.rel_encoder_input_norm(rel_feats)
+
+        node_latents = self.node_encoder(node_feats)
+        rel_latents = self.rel_encoder(rel_feats)
+        if self.effect_normalization != 'none':
+            node_latents = self.node_encoder_norm(node_latents)
+            rel_latents = self.rel_encoder_norm(rel_latents)
+
+        for _ in range(self.num_mp_blocks):
+            for idx, mpri in enumerate(self.mp_rel_idx):
+                relidx = (rel_stages == mpri)  # size R, total number of True is r
+
+                new_rel_latents, agg_new_rel_latents = self.propagate(edge_index=rels[relidx].t()[[1,0]], node_latents=(node_latents, node_latents), rel_latents=rel_latents[relidx], relidx=relidx, num_rels=num_rels, num_nodes=num_nodes, idx=idx)
+                
+                new_node_latents = self.node_processors[idx](torch.cat([node_latents, agg_new_rel_latents], 1))  # N
+                if self.effect_normalization != 'none':
+                    new_node_latents = self.node_processor_norm[idx](new_node_latents)
+
+                node_latents = node_latents + new_node_latents
+                rel_latents = rel_latents + new_rel_latents
+
+        out = self.node_dec(node_latents)
+        return out
+    
+    def message(self, index, node_latents_i, node_latents_j, rel_latents, relidx, num_rels, num_nodes, idx):
+        rli_ltn = self.rel_processors[idx](torch.cat([rel_latents, node_latents_i, node_latents_j], 1))  # r
+        if self.effect_normalization != 'none':
+            rli_ltn = self.rel_processor_norm[idx](rli_ltn)
+        return relidx, rli_ltn, num_rels, num_nodes
+        
+
+    def aggregate(self, inputs, index, dim_size=None):
+        relidx, rli_ltn, num_rels, num_nodes = inputs
+        new_rel_latents = torch_scatter.scatter(rli_ltn, torch.where(relidx)[0], dim=0, dim_size=num_rels)  # R
+        agg_new_rel_latents = torch_scatter.scatter_add(rli_ltn, index, dim=0, dim_size=num_nodes)  # N
+        return new_rel_latents, agg_new_rel_latents
